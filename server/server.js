41a5d09b9bf87700ca75041fd83696d9af31c706
Rotem Sade <rotemsade@Rotems-MacBook-Pro.local>
2018-11-19 14:55:14 +0200

    Greet new user, and alert others about joining & leaving users

--- server/server.js
+++ server/server.js
@@ -15,17 +15,34 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('messageRecived', message);
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createAt: new Date().getTime()
-    })
+    });
   });
 
   socket.on('disconnect', () => {
     console.log('User disconnected from the server');
+    socket.broadcast.emit('newMessage', {
+      from: 'Admin',
+      text: 'User has left',
+      createAt: new Date().getTime()
+    });
   });
 });
 
