5871a8f4e3383d833115f7701876cd0186681573
Rotem Sade <rotemsade@Rotems-MacBook-Pro.local>
2018-11-20 10:28:51 +0200

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const path = require('path');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
 
@@ -23,6 +23,10 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message) => {
     console.log('messageRecived', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,13 +1,25 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
     var generatedMessage = generateMessage('User', 'Test Message');
 
-    expect(generatedMessage.from).toBe('User');
-    expect(generatedMessage.text).toBe('Test Message');
+    expect(generatedMessage).toMatchObject({from: 'User',text: 'Test Message'});
     expect(typeof generatedMessage.createdAt).toBe('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = "User";
+    var latitude = 32.1435253;
+    var longitude = 34.8420176;
+    var url = 'https://www.google.com/maps?q=32.1435253,34.8420176';
+    var generatedLocationMessage = generateLocationMessage(from, latitude, longitude);
+
+    expect(generatedLocationMessage).toMatchObject({from,url});
+    expect(typeof generatedLocationMessage.createdAt).toBe('number');
   });
 });
